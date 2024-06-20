c7660950fb76aa32f72511b7c9d71c1adad95a39
devgabrielsborges <dev.gabrielsborges@gmail.com>
2024-06-20 17:40:45 -0300

    Excel version

--- classes.py
+++ classes.py
@@ -5,6 +5,7 @@ import json
 import tweepy
 
 from datetime import datetime
+from openpyxl import load_workbook
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,104 +14,53 @@ from googleapiclient.errors import HttpError
 from twilio.rest import Client
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
-
 from dotenv import load_dotenv
-class Message:
+
+class SheetXlsx:
 
     @staticmethod   # função para obter horário
     def _data_hora():
         fuso_BR = pytz.timezone("Brazil/East")
         horario_BR = datetime.now(fuso_BR)
         return horario_BR
-    
 
-    def __init__(self, actual_place):
+    def __init__(self, sheetname : str, sheetpath : str, actual_place : int):
+        self.sheetname = sheetname
+        self.sheet = load_workbook(sheetpath)
+        self.worksheet = self.sheet.active
         self.actual_place = actual_place
         self.date = self._data_hora()
         self.values_add = None
-    
-    def base_struct(method):
-        def wrapper(self, *args, **kwargs):
-            # If modifying these scopes, delete the file token.json.
-            SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-            # The ID and range of a sample spreadsheet.
-            SAMPLE_SPREADSHEET_ID = "1Fam3Tf54V0E_IzmEg_OsT9wnqqUlpz-cHl-ShPds2JE"   # ID da planilha
-            SAMPLE_RANGE_NAME = f"Produtos!A{self.actual_place}:D{self.actual_place}"
-
-            """Shows basic usage of the Sheets API.
-            Prints values from a sample spreadsheet.
-            """
-            creds = None
-            # The file token.json stores the user's access and refresh tokens, and is
-            # created automatically when the authorization flow completes for the first
-            # time.
-            if os.path.exists("token.json"):
-                creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-            # If there are no (valid) credentials available, let the user log in.
-            if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
-                else:
-                    flow = InstalledAppFlow.from_client_secrets_file(
-                    r"C:\Users\gsbex\development\xbot\X-Bot\credentials.json", SCOPES
-                    )
-                creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-                with open(r"C:\Users\gsbex\development\xbot\token.json", "w") as token:
-                    token.write(creds.to_json())
-            self.creds = creds
-            self.SAMPLE_SPREADSHEET_ID = SAMPLE_SPREADSHEET_ID
-            self.SAMPLE_RANGE_NAME = SAMPLE_RANGE_NAME
-
-            method(self, *args, **kwargs)
-        return wrapper
-
-    @base_struct
     def get_info(self):
-        try:
-            service = build("sheets", "v4", credentials=self.creds)
+        self.produto = self.worksheet[f"A{self.actual_place}"].value
+        self.valor = self.worksheet[f"B{self.actual_place}"].value
+        self.link = self.worksheet[f"C{self.actual_place}"].value
+        self.info = [self.produto, self.valor, self.link]
+        print(self.info)
+        
+        return self.info
+        
+    def post_info(self, value_add : list) -> list:
 
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            result = (
-                sheet.values()
-                .get(spreadsheetId=self.SAMPLE_SPREADSHEET_ID, range=self.SAMPLE_RANGE_NAME)
-                .execute()
-            )
-            print(result)
-            valores = result['values'][0]
-            print(valores)
-            self.produto = valores[0]
-            self.valor = valores[1]
-            self.link = valores[2]
-            
-        except HttpError as err:
-            print(err)
-    
-    @base_struct
-    def post_info(self, values_add : list, column='E'):
-        try:
-            service = build("sheets", "v4", credentials=self.creds)
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            # Add or edit some info
+        '''   
+        values_add = [status_message, product_message]
+        '''
 
-            values_add = [values_add]
-            result = (
-                sheet.values()
-                .update(spreadsheetId=self.SAMPLE_SPREADSHEET_ID, range=f'{column}{self.actual_place}', valueInputOption="USER_ENTERED", body={'values': values_add})
-                .execute()
-            )
-            print(result)
-                
-            # Método update + valueInputOption
-        except HttpError as err:
-            print(err)
-    
-    @property
-    def info(self):
-        return [self.produto, self.valor, self.link]
+        if len(value_add) == 2:
+            self.worksheet[f"D{self.actual_place}"] = value_add[0]
+            self.worksheet[f"E{self.actual_place}"] = value_add[1]
+            self.worksheet[f"F{self.actual_place}"] = str(self._data_hora())
+
+            self.sheet.save(f"{self.sheetname}.xlsx")
+            print("Worksheet has been saved")
+
+
+        
+    def info(self) -> list:
+        if None not in [self.produto, self.valor, self.link]:
+            return [self.produto, self.valor, self.link]
+
 
 
 class FirebaseAPI:
