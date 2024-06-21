cc037e59163d68ad43d552c74784af8859ee9f60
devgabrielsborges <dev.gabrielsborges@gmail.com>
2024-06-21 17:00:14 -0300

    Lint

--- classes.py
+++ classes.py
@@ -1,99 +1,168 @@
+import json
+from datetime import datetime
 import pytz
 import requests
-import json
 
-from datetime import datetime
 from openpyxl import load_workbook
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from twilio.rest import Client
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
-from dotenv import load_dotenv
+
 
 class SheetXlsx:
+    """ Class for operation with Excel xlsx worksheets
+    """
 
     @staticmethod   # função para obter horário
-    def _data_hora():
-        fuso_BR = pytz.timezone("Brazil/East")
-        horario_BR = datetime.now(fuso_BR)
-        return horario_BR
+    def _data_hora() -> str:
+        return str(datetime.now(pytz.timezone("Brazil/East")))
 
-    def __init__(self, sheetname : str, sheetpath : str, actual_place : int):
+    def __init__(self, sheetname: str, sheetpath: str, actual_place: int):
+        """__init__ _summary_
+
+        Args:
+            sheetname (str): Name of sheet for future operations and saving
+            sheetpath (str): Path of the archive
+            actual_place (int): Index for operations with the cells
+        """
         self.sheetname = sheetname
         self.sheet = load_workbook(sheetpath)
         self.worksheet = self.sheet.active
         self.actual_place = actual_place
         self.date = self._data_hora()
         self.values_add = None
+        self.produto = None
+        self.valor = None
+        self.link = None
+        self.info = None
 
-    def get_info(self):
+    def get_info(self) -> list:
         self.produto = self.worksheet[f"A{self.actual_place}"].value
         self.valor = self.worksheet[f"B{self.actual_place}"].value
         self.link = self.worksheet[f"C{self.actual_place}"].value
         self.info = [self.produto, self.valor, self.link]
-        print(self.info)
-        
         return self.info
-        
-    def post_info(self, value_add : list) -> list:
 
-        '''   
-        values_add = [status_message, product_message]
-        '''
+    def post_info(self, value_add: list):
+        """post_info -> Add informations to the cells
 
+        Args:
+            value_add (list): [status_message, product_message]
+        """
         if len(value_add) == 2:
             self.worksheet[f"D{self.actual_place}"] = value_add[0]
             self.worksheet[f"E{self.actual_place}"] = value_add[1]
-            self.worksheet[f"F{self.actual_place}"] = str(self._data_hora())
+            self.worksheet[f"F{self.actual_place}"] = self._data_hora()
 
             self.sheet.save(f"{self.sheetname}.xlsx")
             print("Worksheet has been saved")
 
 
-        
-    def info(self) -> list:
-        if None not in [self.produto, self.valor, self.link]:
-            return [self.produto, self.valor, self.link]
-
-
-
 class FirebaseAPI:
-    
-    def __init__(self, endpoint : str):
+    """ Class for operations with Firebase
+    """
+
+    def __init__(self, endpoint: str):
+        """__init__ Set up the URL of Firebase Realtime Database
+
+        Args:
+            endpoint (str): URL of Realtime Database
+        """
         self.endpoint = endpoint
-    
-    def get_data(self, page=''):
+
+    def get_data(self, *page: str) -> dict:
+        """get_data getter method for data in Realtime Database
+
+        Args:
+            page (str, optional): URL of an especific page to get the data.
+            Defaults to '' if you want get data from the main page
+
+        Returns:
+            _type_: dict
+        """
         if page != '':
-            request = requests.get(f'{self.endpoint}/{page}/.json')
-        
+            request = requests.get(
+                f'{self.endpoint}/{page}/.json',
+                timeout=10
+            )
+
         else:
-            request = requests.get(f'{self.endpoint}/.json')
-        
+            request = requests.get(
+                f'{self.endpoint}/.json',
+                timeout=10
+            )
+
         return request.json()
 
     def post_data(self, page='', info=''):
+        """post_data setter method for data in Realtime Database
+
+        Args:
+            page (str, optional): URL of an especific page to get the data.
+            Defaults to '' if you want get data from the main page
+
+            info (str, optional): data to post. Defaults to ''.
+        """
         if page != '' and info != '':
-            request = requests.post(f'{self.endpoint}/{page}/.json', data=json.dumps(info))
+            requests.post(
+                f'{self.endpoint}/{page}/.json',
+                data=json.dumps(info),
+                timeout=10
+            )
+
         elif info != '':
-            request = requests.post(f'{self.endpoint}/.json', data=json.dumps(info))
+            requests.post(
+                f'{self.endpoint}/.json',
+                data=json.dumps(info),
+                timeout=10
+            )
 
     def patch_data(self, page='', info=''):
+        """patch_data Patch data in Realtime Database
+
+        Args:
+            page (str, optional): URL of an especific page to get the data.
+            Defaults to '' if you want get data from the main page
+
+            info (str, optional): data to post. Defaults to ''.
+        """
         if page != '' and info != '':
-            request = requests.patch(f'{self.endpoint}/{page}/.json', data=json.dumps(info))
+            requests.patch(
+                f'{self.endpoint}/{page}/.json',
+                data=json.dumps(info),
+                timeout=10
+            )
         elif info != '':
-            request = requests.patch(f'{self.endpoint}/.json', data=json.dumps(info))
+            requests.patch(
+                f'{self.endpoint}/.json',
+                data=json.dumps(info),
+                timeout=10
+            )
 
 
 class TwilioAPI:
-    def __init__(self, account_sid, auth_token):
+    """ class for operations with Twilio SMS API
+    """
+    def __init__(self, account_sid: str, auth_token: str):
+        """__init__ Set the account sid and authenticantion token
+
+        Args:
+            account_sid (str): account sid
+            auth_token (str): authentication token
+        """
         self.account_sid = account_sid
         self.auth_token = auth_token
-    
-    def send_sms(self, from_number : str, to_number : str, msg : str):
+        self.client = None
+        self.sid = None
+
+    def send_sms(self, from_number: str, to_number: str, msg: str):
+        """send_sms _summary_
+
+        Args:
+            from_number (str): Number from Twilio
+            to_number (str): Number you want to send the message.
+            msg (str): Message
+        """
         self.client = Client(self.account_sid, self.auth_token)
         message = self.client.messages.create(
             to=to_number,
@@ -103,18 +172,44 @@ class TwilioAPI:
         self.sid = message.sid
         print(message.sid)
 
+
 class GroqCloud:
-    def __init__(self, job, criativity=0, model="llama3-8b-8192"):
+    """ class for operations with Groq Cloud
+    """
+    def __init__(self, job: str, criativity=0, model="llama3-8b-8192"):
+        """__init__ Defines initial information to run the requests
+
+        Args:
+            job (_type_): Context for the model
+            criativity (int, optional): Temperature parameter. Defaults to 0.
+            model (str, optional): Model. Defaults to "llama3-8b-8192".
+        """
         self.job = job    # informações prévias para o modelo
         self.criativity = criativity   # temperature
         self.model = model   # tipo de modelo
         self.prompt = ChatPromptTemplate.from_messages(
             [("system", f"{self.job}"), ("human", "{text}")]
         )
-        self.chat = ChatGroq(temperature=self.criativity, model_name=f"{self.model}")
-    
-    def request(self, msg):
-        self.chain = self.prompt | self.chat   # conexão
-        self.response = self.chain.invoke({"text": f"{msg}"})   # enviando requisicao
-        
+        self.chat = ChatGroq(
+            temperature=self.criativity,
+            model_name=f"{self.model}"
+        )
+        self.response = None
+        self.chain = None
+
+    def request(self, msg: str) -> str:
+        """request Send a prompt to Groq Cloud
+
+        Args:
+            msg (_type_): Prompt for the chat
+
+        Returns:
+            str: Response of the prompt
+        """
+        # getting conexion
+        self.chain = self.prompt | self.chat
+
+        # sending request
+        self.response = self.chain.invoke({"text": f"{msg}"})
+
         return self.response.content
--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from dotenv import load_dotenv
 from classes import SheetXlsx, FirebaseAPI, TwilioAPI, GroqCloud
 
 db = FirebaseAPI('https://x-bot-borges-default-rtdb.firebaseio.com/')
-page = 'actual_index'
-index = int(db.get_data(page))   # armazena índice atual
+PAGE = 'actual_index'
+index = int(db.get_data(PAGE))   # armazena índice atual
 print(f'Index: {index}\n')
 
 wb = SheetXlsx(r"Projeto-X-Bot", r"Projeto-X-Bot.xlsx", index)
 wb.get_info()   # busca informações da linha a partir do índice
-db.post_data('/Postagens/Horarios', {f'{index}':str(wb.date)})
+db.post_data('/Postagens/Horarios', {f'{index}': str(wb.date)})
 print(wb.date)
 
 load_dotenv()
 
-prompt_msg = f"Imagine que você é um estrategista em posts de tweets sobre promoções imperdíveis. Crie um texto curto e poderoso para vender um/uma {wb.produto} que custa {wb.valor}. O texto deve ser próprio para tweets e deve trazer a ideia de urgência para que os usuários se interessem pelo produto. Lembrando que vendemos produtos de terceiros, não são produtos nossos. Texto em português do Brasil. Até 280 caracteres"
+prompt_msg = (
+    f"Você é um estrategista em posts de tweets sobre promoções imperdíveis."
+    f"Faça um texto curto e eficaz para vender {wb.produto}. Custa {wb.valor}."
+    f"O texto deve ser próprio para tweets e deve trazer a ideia de urgência"
+    f"para que os usuários se interessem pelo produto."
+    f"Lembrando que vendemos produtos de terceiros, não são produtos nossos."
+    f"Texto em português do Brasil. Até 280 caracteres"
+)
 
-groq_chat = GroqCloud("You are especialist at sales and marketing and want to post sales promotions at Twitter",criativity=1.15, model="llama3-70b-8192")
-tweet_body = f"{groq_chat.request(prompt_msg)}\n{wb.link}" 
+groq_chat = GroqCloud(
+    "You're especialist at sales and marketing and post sales promotions",
+    criativity=1.15,
+    model="llama3-70b-8192"
+)
+
+tweet_body = f"{groq_chat.request(prompt_msg)}\n{wb.link}"
 print(tweet_body)
 
 xbot = tweepy.Client(
@@ -34,9 +46,10 @@ xbot.create_tweet(text=tweet_body)
 wb.post_info(['Postado', tweet_body])
 
 new_sms = TwilioAPI(os.getenv('twilio_sid'), os.getenv('twilio_token'))
-sms_body = f"Acabei de postar!\nProduto: {wb.info[0]}\nValor: {wb.info[1]}\n Link {wb.info[2]}"
+sms_body = (
+    f"Acabei de postar!\nProduto: "
+    f"{wb.info[0]}\nValor: {wb.info[1]}\n Link {wb.info[2]}"
+)
 
-db.patch_data(info={'actual_index':index + 1})
+db.patch_data(info={'actual_index': index + 1})
 new_sms.send_sms(os.getenv('number_from'), os.getenv('number_to'),  sms_body)
-
-
